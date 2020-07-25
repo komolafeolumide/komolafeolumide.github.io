a5d40cabe60f73058aa0d25b8703f5eb07897cff
komolafeolumide <dollytee007@gmail.com>
2020-07-25 18:57:48 +0100

    Contact form modified

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,51 +1,24 @@
 <?php
-$errors = '';
-$myemail = 'dollytee007@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  ||
-   empty($_POST['email']) ||
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
 
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
+    $email_from = 'egtpreversity@gmail.com';
 
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
+    $email_subject = "New Form Submission";
 
-if( empty($errors))
-{
-	$to = $myemail;
-	$email_subject = "Contact form submission: $name";
-	$email_body = "You have a new message from Victoriamichaels.org. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message";
+    $email_body = "Name: $name.\n".
+                    "Email: $visitor_email.\n".
+                      "Message: $message.\n";
 
-	$headers = "From: $myemail\n";
-	$headers .= "Reply-To: $email_address";
+    $to = "dollytee007@gmail.com";
 
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
-}
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers)
+
+    headers("Location: contact-form.html")
+
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
-<?php
-echo nl2br($errors);
-?>
-
-
-</body>
-</html>
